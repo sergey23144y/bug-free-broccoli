57ef4dd8010ef7e03534314173656467fb733c4c
fixel <fixelti@gmail.com>
2023-08-11 14:41:23 +0300

    addForeighnKey method created

--- internal/tests/structToSQLFile_test.go
+++ internal/tests/structToSQLFile_test.go
@@ -6,20 +6,29 @@ import (
 )
 
 type User struct {
+	ID   int32 `gorm:"primary_key"`
 	Name string
 	Age  int
 }
 
 type Product struct {
-	UserID      User
-	Name        string
+	ID          int `gorm:"primary_key"`
 	Description string
 	Price       float64
+	UserID      int
+	User        User `gorm:"foreignKey:product:user_id:user:id"`
+}
+
+type Ticket struct {
+	ID        int `gorm:"primary_key"`
+	Price     float64
+	ProductID int
+	Product   Product `gorm:"foreignKey:ticket:product_id:product:id"`
 }
 
 func TestStructToSQLFile(t *testing.T) {
 	db := SebbiaDB.New()
 
-	db.CreateSQLFileMigration("./test_migration/test_00001_init_up.sql", User{}, Product{})
+	db.CreateSQLFileMigration("./test_migration/test_00001_init_up.sql", User{}, Product{}, Ticket{})
 
 }
--- migration.go
+++ migration.go
@@ -1,17 +1,26 @@
 package SebbiaDB
 
 import (
+	"fmt"
 	"github.com/sunary/sqlize"
 	"os"
+	"reflect"
+	"strings"
+)
+
+const (
+	foreignKeyTag = "foreignKey" // необходимо для определения вхождения в значение тега 'gorm'.
+	separator     = ":"          // разделитель для значений тега 'gorm'.
 )
 
 func (d *DBGORM) Migrate(agrs ...interface{}) {}
 
-func (d *DBGORM) CreateSQLFileMigration(path string, args ...interface{}) error {
-	newMigration := sqlize.NewSqlize(sqlize.WithSqlTag("psql"), sqlize.WithMigrationFolder(""))
+func (d *DBGORM) createSQLFileMigration(path string, args ...interface{}) error {
+	newMigration := sqlize.NewSqlize(sqlize.WithSqlTag("gorm"), sqlize.WithMigrationFolder(""))
 
 	for i := 0; i < len(args); i++ {
 		_ = newMigration.FromObjects(args[i])
+		foreignKey := d.addForeignKey(args[i])
 
 		f, err := os.Create(path)
 
@@ -21,7 +30,7 @@ func (d *DBGORM) CreateSQLFileMigration(path string, args ...interface{}) error 
 
 		defer f.Close()
 
-		_, err2 := f.WriteString(newMigration.StringUp() + "\n")
+		_, err2 := f.WriteString("\n" + newMigration.StringUp() + foreignKey + "\n")
 
 		if err2 != nil {
 			return err
@@ -29,4 +38,28 @@ func (d *DBGORM) CreateSQLFileMigration(path string, args ...interface{}) error 
 	}
 
 	return nil
+}
+
+func (d *DBGORM) addForeignKey(table interface{}) (foreignKey string) {
+	val := reflect.ValueOf(table) // в val заноситься реализация конректного типа table
+	valType := val.Type()         // определение типа перменной val. Переменная valType становится типом данных
+
+	// итерируемся по количеству полей в структуре
+	//TODO: паникует, если valType не структура. Надо исправить.
+	for i := 0; i < valType.NumField(); i++ {
+		field := valType.Field(i)         // получаем поле, по его номеру
+		fieldTag := field.Tag.Get("gorm") // получаем значение тега 'gorm'. Считай, как в hash map
+
+		// проверка, что в значениях тега 'gorm'
+		// входит 'foreignKey'
+		if strings.Contains(fieldTag, foreignKeyTag) {
+			splitTag := strings.Split(fieldTag, separator) // разделяем значение тега на массив
+
+			foreignKey = fmt.Sprintf(
+				" ALTER TABLE '%s' ADD FOREIGN KEY (%s) REFERENCES '%s' (%s)",
+				splitTag[1], splitTag[2], splitTag[3], splitTag[4])
+		}
+	}
+
+	return
 }
